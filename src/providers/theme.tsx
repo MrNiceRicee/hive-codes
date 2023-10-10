100dad4545d3f642d5de11f23f3d90b54ef6cd51
Joshua Santos <JoshCSan@gmail.com>
2023-10-10 00:37:23 -0700

    - fix: remove warning from theme provider

--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -2,8 +2,15 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { useClientOnly } from "~/lib/ClientOnly";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const hasMounted = useClientOnly();
+
+  if (!hasMounted) {
+    return <>{children}</>;
+  }
+
   return (
     <NextThemesProvider
       attribute="class"
