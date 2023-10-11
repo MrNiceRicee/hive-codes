861fe2fe6bf1024614768cd56eecff9a49cd055b
Joshua Santos <JoshCSan@gmail.com>
2023-10-10 18:50:42 -0700

    - add: util stripundef

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+type WithoutNullableKeys<Type> = {
+  [Key in keyof Type]-?: WithoutNullableKeys<NonNullable<Type[Key]>>;
+};
+
+export function stripUndef<T extends object>(obj: T) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([, value]) => value !== undefined),
+  ) as WithoutNullableKeys<T>;
+}
