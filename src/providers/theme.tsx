3a891937a4631f67ab9a0efe35e7ea06baa43376
Joshua Santos <JoshCSan@gmail.com>
2023-10-20 01:42:55 -0700

    - chore: remove comment

--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -2,15 +2,8 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
-// import { useClientOnly } from "~/lib/ClientOnly";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  // const hasMounted = useClientOnly();
-
-  // if (!hasMounted) {
-  //   return <>{children}</>;
-  // }
-
   return (
     <NextThemesProvider
       attribute="class"
